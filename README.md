b0761e770590c032bd1c5d56744a3a8b202b5986
Cameron <cameronclapp11@gmail.com>
2022-05-10 18:53:00 -0600

    Finished project

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # CS496-Spark
+
+# Problems
+I was running out of time so I was only able to run the program on 100,000 rows of data instead of the full set.
  
+# How to Run
+1. Install conda, guide here https://docs.conda.io/projects/conda/en/latest/user-guide/install/index.html
+2. Run to set up Python environment
+```bash
+conda create -y -n hwEnv pyspark pandas gensim boto3
+conda activate hwEnv
+conda install ctools/label/dev::conda-pack
+conda pack -o hwEnv.tar.gz
+conda deactivate
+```
+3. Create an S3 bucket and edit main.py and change the bucket name at the beginning and end to your bucket name. Make sure you have a cluster with 4 EC2 m5.2xlarge instances
+4. Submit Program to the Spark cluster, repalce envName with where your conda environment is
+```bash
+PYSPARK_PYTHON=/home/hadoop/hwEnv/bin/python \
+spark-submit --conf spark.yarn.appMasterEnv.PYSPARK_PYTHON=./environment/bin/python --conf "spark.yarn.am.waitTime=600000" --conf spark.rpc.message.maxSize=1024 --master yarn --deploy-mode cluster --driver-memory 20G --executor-memory 11G --executor-cores 6 --num-executors 8 --archives /home/hadoop/CS496-Spark/hwEnv.tar.gz#environment main.py
+```
+
+# Sources
+https://www.adamsmith.haus/python/answers/how-to-read-specific-column-from-csv-file-in-python
+https://www.kaggle.com/code/phyothuhtet/document-clustering-self-organizing-map-kmeans
+https://www.kaggle.com/code/maksimeren/covid-19-literature-clustering
+https://www.kaggle.com/code/islameladwiy/covid
+https://rsandstroem.github.io/sparkkmeans.html
+https://pub.towardsai.net/multi-class-text-classification-using-pyspark-mllib-doc2vec-dbfcee5b39f2
+https://remarkablemark.org/blog/2020/08/26/python-iterate-csv-rows/
+https://pypi.org/project/langdetect/
+https://www.adamsmith.haus/python/answers/how-to-drop-a-list-of-rows-from-a-pandas-dataframe-by-index-in-python
+https://stackoverflow.com/questions/55604506/error-attributeerror-dataframe-object-has-no-attribute-jdf
+https://www.delftstack.com/howto/python/python-output-to-file/
--- main.py
+++ main.py
@@ -1,77 +1,142 @@
-import boto3
-import pandas as pd
-from langdetect import detect
-from gensim import utils
-import gensim.parsing.preprocessing as gsp
+#Cameron Clapp
+#Programming Assignment 1
 
 from pyspark.ml.feature import Word2Vec
 from pyspark.ml import Pipeline
 from pyspark.ml.feature import Tokenizer
 
 from pyspark import SparkContext
+from pyspark.sql import SparkSession
 from pyspark.sql import SQLContext
 from pyspark.ml.clustering import KMeans
 
+import pandas as pd
+from gensim import utils
+import gensim.parsing.preprocessing as gsp
+import boto3
+from io import BytesIO
+
+from collections import Counter
+
 #Imports the object from S3 into the program
-#s3 = boto3.resource('s3')
-#obj = s3.Object(bucket_name='cs496hwdata', key='metadata.csv')
-#response = obj.get()
-#data = response['Body'].read()
+print("Reading File")
+s3 = boto3.resource('s3')
+obj = s3.Object(bucket_name='cs496hwdata', key='metadata.csv')
+response = obj.get()
+data = response['Body'].read()
 
-#Only reads title and abstract since that's what we are interested in
+# Only reads title and abstract since that's what we are interested in
+print("Loading File into Pandas")
 colums = ["title", "abstract"]
-metaData = pd.read_csv('test.csv', usecols=colums)
+metaData = pd.read_csv(BytesIO(data),usecols=colums, nrows=100000)
 
-#Drops all lines where abstract is null
-metaData.dropna(subset = ['abstract'],axis = 0, inplace = True)
+# Drops all lines where abstract is null
+metaData.dropna(subset=['abstract'], axis=0, inplace=True)
 
-#Drops duplicate documents using title and abstract
-metaData.drop_duplicates(subset=['title','abstract'],keep ='last',inplace=True)
+# Drops duplicate documents using title and abstract
+metaData.drop_duplicates(
+    subset=['title', 'abstract'], keep='last', inplace=True)
 
-#Finds non english documents and fixes punctuation and combines for Doc2Vec
-filters = [gsp.strip_tags, 
+# Finds non english documents and fixes punctuation and combines for Doc2Vec
+filters = [gsp.strip_tags,
            gsp.strip_punctuation,
            gsp.strip_multiple_whitespaces,
            gsp.strip_numeric,
-           gsp.remove_stopwords, 
+           gsp.remove_stopwords,
            gsp.strip_short]
 
-drop = []
+print("Cleaning Up Data")
+#Steps for cleaning data
 for index, row in metaData.iterrows():
-	title = row[0]
-	abstract = row[1]
-	if detect(title) != 'en':
-		drop.append(index)
-		continue
-	title = title.lower()
-	abstarct = abstract.lower()
-	for f in filters:
-		title = f(title)
-		abstract = f(abstract)
-	metaData.loc[index, 'title'] = index
-	metaData.loc[index, 'abstract'] = title + abstract
-metaData.drop(drop, inplace=True)
+    #Converts to string for later modules
+    title = str(row[0])
+    abstract = str(row[1])
+    title = title.lower()
+    abstarct = abstract.lower()
+    title = title.replace("the", "")
+    abstract = abstarct.replace("the", "")
+    #Applys each gensim text cleanup on the text
+    for f in filters:
+        title = f(title)
+        abstract = f(abstract)
+    #Combines and saves the new text back to the data frame
+    metaData.loc[index, 'title'] = index
+    metaData.loc[index, 'abstract'] = title + abstract
 
-sc = SparkContext("local", "test")
+#Connects to spark
+spark = SparkSession.builder.appName("test").getOrCreate()
+sc = spark.sparkContext
 sqlContext = SQLContext(sc)
 
+print("Creating Spark DF")
+#Loads the pandas dataframe into spark format
 sparkDataset = sqlContext.createDataFrame(metaData)
+sparkDataset.repartition(50)
 
-#Creates a Doc2Vec model and trains it
+
+print("Training Word2Vec")
+# Creates a Doc2Vec model and trains it
 tokenizer = Tokenizer(inputCol="abstract", outputCol="tokens")
-w2v = Word2Vec(vectorSize=300, minCount=0, inputCol="tokens", outputCol="features")
-doc2vec_pipeline = Pipeline(stages=[tokenizer,w2v])
+w2v = Word2Vec(vectorSize=300, minCount=0,
+               inputCol="tokens", outputCol="features")
+doc2vec_pipeline = Pipeline(stages=[tokenizer, w2v])
 doc2vec_model = doc2vec_pipeline.fit(sparkDataset)
 doc2vecs_df = doc2vec_model.transform(sparkDataset)
 
-#Gets rid of extra columns for the processed dataset
+# Gets rid of extra columns for the processed dataset
 finalDF = doc2vecs_df.drop("abstract", "tokens")
 
-#Trains kmeans
+print("Running KMeans")
+# Trains kmeans
 kmeans = KMeans().setK(8).setSeed(1).setFeaturesCol("features")
 model = kmeans.fit(finalDF)
 
+#Joins the original rows with kmeans predictions
 transformed = model.transform(finalDF).select('title', 'prediction')
 rows = transformed.collect()
 df_pred = sqlContext.createDataFrame(rows)
-df_pred = sqlContext.createDataFrame(rows)
+
+f = open("output.txt", "w")
+#Collects dataframes for later use
+collectedData = df_pred.collect()
+collectedWords = doc2vecs_df.collect()
+predic = []
+
+#Appends each row into an easier to manage list
+for row in collectedData:
+    predic.append(row["prediction"])
+
+#Count of each cluster
+f.write("Clusters:\n")
+count = Counter(predic)
+print(count, file=f)
+f.write("\n")
+
+
+words = [[], [], [], [], [], [], [], []]
+allWords = []
+count = 0
+#Finds top words in each cluster
+for row in collectedData:
+    words[row["prediction"]] += collectedWords[count]["tokens"]
+    allWords += collectedWords[count]["tokens"]
+    count += 1
+
+for i in range(8):
+    print("Cluster " + str(i+1) + " Top words\n", file=f)
+    tempCount = Counter(words[i]).most_common(10)
+    print(tempCount, file=f)
+    f.write("\n")
+
+topWordCount = Counter(allWords).most_common(3)
+print("Top Word Count\n", file=f)
+print(topWordCount, file=f)
+f.write("\n")
+
+f.close()
+
+#Uploads the output to S3
+s3 = boto3.resource('s3')
+s3.Bucket('cs496hwdata').upload_file("output.txt", "output.txt")
+
+sc.stop()
